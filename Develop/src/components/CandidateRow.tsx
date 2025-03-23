262e0011a99c3288e38e6b58e04442a6ecb2015a
ikealmighty <isaacyates7@gmail.com>
2025-03-23 16:22:37 -0500

    implemented a single row for potentials table

---
+++ Develop/src/components/CandidateRow.tsx
@@ -0,0 +1,35 @@
+import "./CandidateRow.module.css";
+import RoundButton from "./RoundButton";
+
+export default function CandidateRow(props: any) {
+  const { candidate, onMinus } = props;
+  return (
+    <>
+      <tr>
+        <td>
+          <img
+            src={candidate.avatar_url}
+            alt={`avatar for ${candidate.login}`}
+          />
+        </td>
+        <td>
+          {candidate.name} ({candidate.login})
+        </td>
+        <td>{candidate.location}</td>
+        <td>
+          <a href={`mailto:${candidate.email}`}>{candidate.email}</a>
+        </td>
+        <td>{candidate.company}</td>
+        <td>{candidate.bio}</td>
+        <td>
+          <RoundButton
+            style={{ backgroundColor: "red" }}
+            onClick={() => onMinus(candidate)}
+          >
+            -
+          </RoundButton>
+        </td>
+      </tr>
+    </>
+  );
+}
